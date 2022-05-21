810ccc1f67610fb8a4bcec617f811dc2658ad5fb
Cristoper Yarleque <caypsaturno@gmail.com>
2022-05-20 23:23:59 -0500

    chore: update

--- src/components/medal/Medal.jsx
+++ src/components/medal/Medal.jsx
@@ -11,13 +11,17 @@ export default function Medal() {
 
   return ( 
   <div>
-    {medallasCristo.length > 0 ? (<div className="container d-flex justify-content-center flex-column align-items-center mt-5 mb-5">
+    {medallasCristo.length > 0 ? (<div className="container 
+    d-flex justify-content-center flex-column align-items-center 
+    mt-5 mb-5">
     <h4><u>Medallas Ganadas</u></h4>
     <div className="row d-flex justify-content-center">
       {medallasCristo.map(({id,nombre,descripcion,imagen}) => (
-        <div key={id} className="card d-flex justify-content-center text-center bg-dark m-2
-        col-xs-8 col-sm-8 col-md-6 col-lg-4">
-        <img src={imagen} alt={nombre} className="img-titulo"  />
+        <div key={id} className="card d-flex justify-content-center text-center bg-dark m-1
+        col-4 col-sm-10 col-md-10 col-lg-4">
+          <div className="d-flex justify-content-center">
+        <img src={imagen} alt={nombre} className="img-titulo" width="100px" />
+        </div>
         <div className="card-body text-light card-cont">
           <h4 className="card-title">{nombre}</h4>
           <p className="card-text small text-secondary">{descripcion}</p>
